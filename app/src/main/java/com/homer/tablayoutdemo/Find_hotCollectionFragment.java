d8894a858bda726be53d977666ace818f41c0f2a
xuhainuo <xuhainuo@cri.cn>
2016-07-28 16:46:28 +0800

    Initial commit

---
+++ app/src/main/java/com/homer/tablayoutdemo/Find_hotCollectionFragment.java
@@ -0,0 +1,19 @@
+package com.homer.tablayoutdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Homer on 2016/6/24.
+ */
+public class Find_hotCollectionFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment1, container, false);
+    }
+}
---
+++ app/src/main/java/com/homer/tablayoutdemo/Find_hotRecommendFragment.java
@@ -0,0 +1,20 @@
+package com.homer.tablayoutdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Homer on 2016/6/24.
+ */
+public class Find_hotRecommendFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment1, container, false);
+        return view;
+    }
+}
